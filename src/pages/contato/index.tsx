2bba2f17bc5d7ff01b42ef3713050fe3bd8801f9
Gabriel Max <gmaxgomes@gmail.com>
2022-08-23 22:32:09 -0300

    add sass

---
+++ src/pages/contato/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./contatoStyle.module.css";
+
+function contato() {
+  return (
+    <h1 className={styles.fulfilled}>contato</h1>
+  )
+}
+
+export default contato;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import style from "../styles/styles.module.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={style.title}>next app <span>hey</span></h1>
+    </div>
+  )
+}
